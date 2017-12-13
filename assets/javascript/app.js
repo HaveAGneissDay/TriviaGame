2aa975ef8b9a4e56e139debba4c84ed666109a4c
David Cheng <dkcheng10@gmail.com>
2017-12-13 14:34:53 -0800

    finished laying down the ground work

---
+++ assets/javascript/app.js
@@ -0,0 +1,86 @@
+//STEP !: Must load the title screen   (    )
+//Step 2: Must load questions X8  (   )
+//STEP 3: Make a Timer to countdown time left to answer questions      (    )
+//STEP 3: Display Score screen    (   )
+
+$(document).ready(function() {
+
+//Global Variables
+
+var correct = 0;
+var incorect = 0;
+var unanswered = 0;
+
+$("#start").on("click", function () {
+
+  $("#display").remove();
+  // should clear webpage
+  // load questions
+  // start timer
+});
+
+
+
+//timer
+
+//Questions
+
+// array of Objects
+//append?
+
+var questions = [
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  {
+    "Question" :
+    "Answers" :
+    "Correct Answer" :
+  },
+  
+]
+
+
+})
